448439ccdf9d92ae3aeff227cd070fcec96ef317
RaphaelCapone <44115221+RaphaelCapone@users.noreply.github.com>
2019-09-22 13:25:40 +0300

    Removed some things up

--- Database/elypsgm_nulled.sql
+++ Database/elypsgm_nulled.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 22, 2019 at 12:06 PM
+-- Generation Time: Jul 30, 2019 at 05:36 PM
 -- Server version: 10.1.40-MariaDB
 -- PHP Version: 7.3.5
 
@@ -32,25 +32,11 @@ CREATE TABLE `accounts` (
   `ID` int(11) NOT NULL,
   `username` text NOT NULL,
   `password` varchar(129) NOT NULL,
-  `score` int(11) NOT NULL DEFAULT '1',
   `email` varchar(85) NOT NULL,
-  `faction` int(11) NOT NULL DEFAULT '0',
   `spawn_location` int(11) NOT NULL DEFAULT '1',
   `age` int(11) NOT NULL,
   `date_registred` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `skin` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `map_icons`
---
-
-CREATE TABLE `map_icons` (
-  `id` int(11) NOT NULL,
-  `type` int(11) NOT NULL,
-  `position` tinytext NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -64,12 +50,6 @@ ALTER TABLE `accounts`
   ADD PRIMARY KEY (`ID`);
 
 --
--- Indexes for table `map_icons`
---
-ALTER TABLE `map_icons`
-  ADD PRIMARY KEY (`id`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -78,12 +58,6 @@ ALTER TABLE `map_icons`
 --
 ALTER TABLE `accounts`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `map_icons`
---
-ALTER TABLE `map_icons`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
